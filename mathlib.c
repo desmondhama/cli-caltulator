d40e56b6d582a89cc1f83ff350cf5510e5a7675b
desmondhama <desmondhama@gmail.com>
2024-11-17 21:46:14 +0000

    iogyi79 C;: C :wD D D ddd B B B B B B B

---
+++ mathlib.c
@@ -0,0 +1,17 @@
+#include "mathlib.h"
+
+int Sum(int a,int b) {
+  return a + b;
+}
+
+int Subtract(int a,int b) {
+  return a - b;
+}
+
+int Multiply(int a,int b) {
+  return a * b;
+}
+
+int Divide(int a,int b) {
+  return a / b;
+}
