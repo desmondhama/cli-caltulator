db712f416b350fbf2170e47644245c4ec787085f
GlebyGlobster <57631406+M0j0-Jr@users.noreply.github.com>
2025-02-10 14:24:16 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
 #include <signal.h>
 
-// Declare the functions
-char *GetUserInput(char operator, int value1, int value2);
-int GiveResultOfCalculation(char operator, int value1, int value2);
 
-int main() {
-  char operator = '+'; // Example operator
-  int value1 = 10, value2 = 20;
-  int running = 1;
-
-  while (running == 1) {
-    printf("Enter First Number, Please!\n");
-    scanf("%d", &value1);
-    printf("Enter Operator, Please!\n");
-    scanf(" %c", &operator);
-    printf("Enter Second Number, Please!\n");
-    scanf("%d", &value2);
-
-    printf("User Input: %s\n", GetUserInput(operator, value1, value2));
-    printf("Calculation Result: %d\n\n", GiveResultOfCalculation(operator, value1, value2));
-  }
-  return 0;
-}
-
-// Define the functions
+/* # FUNCTIONS # */
 char *GetUserInput(char operator, int value1, int value2) {
-  static char result[100];
-  sprintf(result, "%d %c %d", value1, operator, value2);
-  return result;
+        static char result[100];
+        sprintf(result, "%d %c %d", value1, operator, value2);
+        return result;
 }
 
 int GiveResultOfCalculation(char operator, int value1, int value2) {
-  switch (operator) {
-    case '+':
-        return value1 + value2;
-    case '-':
-        return value1 - value2;
-    case '*':
-        return value1 * value2;
-    case '/':
-        return (value2 != 0) ? value1 / value2 : 0;
-    default:
-        printf("ERROR: Invalid Operator!\n");
-        printf("Valid options are: +, -, *, /.");
+        switch (operator) {
+                case '+':
+                        return value1 + value2;
+                case '-':
+                        return value1 - value2;
+                case '*':
+                        return value1 * value2;
+                case '/':
+                        return (value2 != 0) ? value1 / value2 : 0;
+                default:
+                        printf("ERROR: Invalid Operator!\n");
+                        printf("Valid options are: +, -, *, /.");
+                        return 0;
+        }
+}
+
+
+/* # MAIN FUNCTION # */
+int main() {
+
+        /* # VARIABLES # */
+        char operator = '+'; // Example operator
+        int value1 = 10, value2 = 20;
+        int running = 1;
+
+        /* # WHILE LOOP # */
+        while (running == 1) {
+                /* # GETTING INPUT # */
+                printf("Enter First Number, Please!\n");
+                scanf("%d", &value1);
+                printf("Enter Operator, Please!\n");
+                scanf(" %c", &operator);
+                printf("Enter Second Number, Please!\n");
+                scanf("%d", &value2);
+
+                /* # PROVIDING CALCULATION RESULT # */
+                printf("User Input: %s\n", GetUserInput(operator, value1, value2));
+                printf("Calculation Result: %d\n\n", GiveResultOfCalculation(operator, value1, value2));
+        }
+
         return 0;
-    }
 }
